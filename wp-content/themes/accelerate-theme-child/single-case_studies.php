fb3a6ab7d82e69411b144fbd6b653cf077e1780f
Sally Lit <sallylitcodes@gmail.com>
2018-06-22 10:26:21 -0700

    added custom case studies pages

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for case study pages
+ *
+ * *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+			<article class="case-study">
+				<?php the_content(); ?>
+			</article>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+		
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
