8458a6fa76a24ab3ab3c56db8bf6cf83c19832c3
Sally Lit <sallylitcodes@gmail.com>
2018-06-26 09:45:14 -0700

    added case studes

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -19,12 +19,39 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
-	<section class="recent-posts">
+	<section class="featured-work">
+		<div class="site-content">
+			<h4>Featured Work</h4>
+
+			<div class="homepage-featured-work-row">
+			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+				<?php while ( have_posts() ) : the_post();
+					$image_1 = get_field("image_1");
+					$size = "medium";
+				?>
+				<div class="individual-featured-work">
+					<figure><a href="<?php the_permalink(); ?>">
+						<?php echo wp_get_attachment_image($image_1, $size); ?></a>
+					</figure>
+
+					<h3 class="individual-work-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+				</div>
+				<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+		</div>
+
+		</div>
+	</section>
+
+
+	
+
+	<section class="recent-posts clear-fix">
 		<div class="site-content">
 			<div class="blog-post">
 				<h4>From the Blog</h4>
